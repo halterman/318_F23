a8c0d13ad1da14e3fa1b9342530f563c7155b673
Rick Halterman <halterman@southern.edu>
2023-09-01 11:50:54 -0400

    Version: Fri Sep  1 11:50:50 EDT 2023

--- 2023-08-30/randrange.cpp
+++ 2023-08-30/randrange.cpp
@@ -2,11 +2,11 @@
 #include "randrange.h"
 
 
-// Returns a pseudorandom integer in the range low...high, inclusive
+// Returns a pseudorandom integer in the range low...high - 1, inclusive
 int randrange(int low, int high) {
     static std::random_device rd;
     static std::mt19937 mt_generator(rd());
-    std::uniform_int_distribution<int> dist(low, high);
+    std::uniform_int_distribution<int> dist(low, high - 1);
     return dist(mt_generator);
 }
 
--- 2023-08-30/randrange.h
+++ 2023-08-30/randrange.h
@@ -1,7 +1,7 @@
 #ifndef RANDRANGE_H_DEFINED
 #define RANDRANGE_H_DEFINED
 
-// Returns a pseudorandom integer in the range low...high, inclusive
+// Returns a pseudorandom integer in the range low...high - 1, inclusive
 int randrange(int low, int high);
 
 #endif
